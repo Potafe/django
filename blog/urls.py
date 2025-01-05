01421be84fc55f9aaa654ff45f5e884839b63a2f
Potafe <139246033+Potafe@users.noreply.github.com>
2025-01-05 21:44:31 +0530

    feat: CRUD posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PostListView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name = "blog-home"),
+    path('', views.PostListView.as_view(), name = "blog-home"),
     path('about/', views.about, name = "blog-about"),
+    path('post/new/', views.PostCreateView.as_view(), name = "post-create"),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name = "post-detail"),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name = "post-update"),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = "post-delete"),
 ]
